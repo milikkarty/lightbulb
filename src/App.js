2398e886dac6db0d14fc5be1105b9d63651b80a0
milikkarty <milikkarty@gmail.com>
2022-07-17 17:33:59 +0300

    Add bulbs and the switch handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
+import bulbOn from './assets/bulb-on.png';
+import bulbOff from './assets/bulb-off.png';
+import bulbBroken from './assets/bulb-broken.png';
+
 function App() {
+
+  const [isOn, setIsOn] = useState(true);
+
+  const handleSwitch = () => {
+    setIsOn(isOn => !isOn);
+  };
+
   return (
     <div className="App">
       <h1>Lightbulb</h1>
+        <img
+          src={isOn ? bulbOn : bulbOff}
+          alt={isOn ? "Light is on." : "Light is off."}
+          onClick={handleSwitch}
+        />
+        <img
+          src={bulbBroken}
+          alt="Broken bulb."
+        />
     </div>
   );
 }

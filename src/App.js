a3c9cebd00c39339a6378594303153c9998616e0
milikkarty <milikkarty@gmail.com>
2022-07-17 23:43:30 +0300

    Add ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { TbBulb, TbBulbOff, TbCookie } from 'react-icons/tb';
+import { ErrorBoundary } from 'react-error-boundary';
 
-function App() {
-
+function Content () {
   const [isOn, setIsOn] = useState(true);
   const [isCookiePressed, setIsCookiePressed] = useState(false);
 
@@ -10,14 +10,15 @@ function App() {
     throw new Error('Don\'t touch it. It\'s for Christmas.');
   };
 
-  const bgColor = isOn ? 'white' : 'black';
-  const color = isOn ? 'black' : 'white';
   const iconStyle = {
     cursor: 'pointer',
     height: '3rem',
     margin: 7,
     width: '2rem'
   }
+
+  const bgColor = isOn ? 'white' : 'black';
+  const color = isOn ? 'black' : 'white';
 
   const handleSwitch = () => {
     setIsOn(isOn => !isOn);
@@ -28,7 +29,7 @@ function App() {
   };
 
   return (
-    <div className="App" style={{ background: bgColor, color: color }}>
+    <div className="content" style={{ background: bgColor, color: color }}>
       <header>
         <span>
           {
@@ -54,6 +55,25 @@ function App() {
           I remember that the Gabilan Mountains to the east of the valley were light gay mountains full of sun and loveliness and a kind of invitation, so that you wanted to climb into their warm foothills almost as you want to climb into the lap of a beloved mother. They were beckoning mountains with a brown grass love. The Santa Lucias stood up against the sky to the west and kept the valley from the open sea, and they were dark and brooding — unfriendly and dangerous. I always found in myself a dread of west and a love of east. Where I ever got such an idea I cannot say, unless it could be that the morning came over the peaks of the Gabilans and the night drifted back from the ridges of the Santa Lucias. It may be that the birth and death of the day had some part in my feeling about the two ranges of mountains.
         </p>
       </article>
+    </div>
+  );
+}
+
+function ErrorFallback ({ error, resetErrorBoundary }) {
+  return (
+    <header className="errorHeader">
+      <p>{error.message}</p>
+      <button onClick={resetErrorBoundary}>A'ight</button>
+    </header>
+  );
+}
+
+function App () {
+  return (
+    <div className="App">
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Content />
+      </ErrorBoundary>
     </div>
   );
 }
